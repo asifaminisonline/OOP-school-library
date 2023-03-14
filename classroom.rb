ab582cac5b854e5ee238ec7ff63f8db224a0a29b
Asif Amin <asifaminisonline@gmail.com>
2023-03-14 19:02:35 +0530

    Add changes as per code reviewer

--- classroom.rb
+++ classroom.rb
@@ -1,11 +1,10 @@
 class Classroom
-  attr_accessor :label, :students
-
+  attr_accessor :label
+  attr_reader :students
   def initialize(label)
     @label = label
     @students = []
   end
-
   def add_student(student)
     @students << student
     student.classroom = self
