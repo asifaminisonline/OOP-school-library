a214aa5a58ae1fd97f04673724d5f002c17b000e
Botlhale Setou <botlhale.setou@gmail.com>
2023-03-21 15:40:09 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -44,41 +44,41 @@ class App
     end
   end
 
-#   def create_tasks(choice)
-#     case choice
-#     when 3
-#       create_person
-#     when 4
-#       create_book
-#     when 5
-#       create_rental
-#     end
-#   end
+  def create_tasks(choice)
+    case choice
+    when 3
+      create_person
+    when 4
+      create_book
+    when 5
+      create_rental
+    end
+  end
 
-#   def create_person
-#     person = CreatePerson.new(@people)
-#     person.create_person
-#   end
+  def create_person
+    person = CreatePerson.new(@people)
+    person.create_person
+  end
 
-#   def create_book
-#     CreateBook.new.create_book(@books)
-#   end
+  def create_book
+    CreateBook.new.create_book(@books)
+  end
 
-#   def list_people
-#     @people.each_with_index do |person, index|
-#       puts "#{index + 1}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-#     end
-#   end
+  def list_people
+    @people.each_with_index do |person, index|
+      puts "#{index + 1}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+  end
 
-#   def list_books
-#     ListBooks.new.list_books(@books)
-#   end
+  def list_books
+    ListBooks.new.list_books(@books)
+  end
 
-#   def create_rental
-#     CreateRental.new(@people, @books, @rentals).create_rental
-#   end
+  def create_rental
+    CreateRental.new(@people, @books, @rentals).create_rental
+  end
 
-#   def list_rental
-#     ListRental.new.list_rental(@rentals)
-#   end
-# end
+  def list_rental
+    ListRental.new.list_rental(@rentals)
+  end
+end
