e18a10b32b443164899c34f490ba4153294c460d
Botlhale Setou <botlhale.setou@gmail.com>
2023-03-21 15:51:30 +0200

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,21 +1,21 @@
-# require_relative './person'
+require_relative './person'
 
-# class Student < Person
-#   attr_reader :classroom, :age, :parent_permission
-#   attr_accessor :type
+class Student < Person
+  attr_reader :classroom, :age, :parent_permission
+  attr_accessor :type
 
-#   def initialize(classroom, age, name, parent_permission, type = 'Student')
-#     super(age, name, parent_permission)
-#     @classroom = classroom
-#     @type = type
-#   end
+  def initialize(classroom, age, name, parent_permission, type = 'Student')
+    super(age, name, parent_permission)
+    @classroom = classroom
+    @type = type
+  end
 
-#   def play_hooky
-#     '¯(ツ)/¯'
-#   end
+  def play_hooky
+    '¯(ツ)/¯'
+  end
 
-#   def classroom=(classroom)
-#     @classroom = classroom
-#     classroom.students.push(self) unless classroom.students.include?(self)
-#   end
-# end
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+end
