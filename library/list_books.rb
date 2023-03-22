ff01a6d7c115cbb174ab2effda96098f5253d88b
asifaminisonline <asifaminisonline@gmail.com>
2023-03-22 13:17:21 +0530

    resolve conflicts

--- library/list_books.rb
+++
@@ -1,8 +0,0 @@
-class ListBooks
-  def list_books(books)
-    @books = books
-    @books.each_with_index do |book, index|
-      puts "#{index + 1}) Title: #{book.title}, Author: #{book.author}"
-    end
-  end
-end
